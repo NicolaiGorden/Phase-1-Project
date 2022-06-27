18d65acf20d658a9fd3943b7d1ab1cb1cc223129
Nicolai Gorden <nicolaigorden@gmail.com>
2022-06-27 14:52:00 -0500

    initialized weapons

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOM Content Loaded!")
+    console.log(document.getElementById('primary').innerHTML)
     retrieveData();
 })
 
 function retrieveData(){
     fetch('http://localhost:3000/weapons')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => initializeWeapons(data))
+}
+//data.ars[0].name
+//data.smgs[0].name
+
+function initializeWeapons(weapons){
+    const primary = `${weapons.ars[0].name.toUpperCase()}
+    <img src=${weapons.ars[0].image}>`
+    const secondary = `${weapons.smgs[0].name.toUpperCase()}
+    <img src=${weapons.smgs[0].image}>`
+    const primaryBox = document.getElementById('primary')
+    const secondaryBox = document.getElementById('secondary')
+
+    primaryBox.innerHTML = primary
+    secondaryBox.innerHTML = secondary
+}
+
+function initializeBackpack(){
+    
 }
